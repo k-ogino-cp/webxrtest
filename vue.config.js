5b10c004c00ed19c5171e6ac9da9a462cef8085e
ogino4101 <ogino@cpnet.co.jp>
2024-04-19 16:45:26 +0900

    [update]

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/product-frontend/' : '/',
-  outputDir: 'docs'
-})
+module.exports = {
+  outputDir: 'docs',
+  assetsDir: './',
+  publicPath: './',
+}
